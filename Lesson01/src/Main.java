80ad8bbe812eb7d8b48d4f1ad4613ad05417a2f5
LuccasMatias <lucas.ma.goes@gmail.com>
2023-08-08 20:24:58 -0300

    02: Data structures Part 1 (If-else)
    1). Write a program to read an integer, and then say if this number is negative or not

--- Lesson01/src/Main.java
+++ Lesson01/src/Main.java
@@ -1,22 +1,18 @@
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
 
+        int input;
         Scanner scan = new Scanner(System.in);
-        int employee, workedHours;
-        double hourValue;
 
-        System.out.println("Enter the employee ID: ");
-        employee = scan.nextInt();
-        System.out.println("\nEnter the worked hours: ");
-        workedHours = scan.nextInt();
-        System.out.println("\nEnter the hour value");
-        hourValue = scan.nextDouble();
-
-        hourValue = hourValue * workedHours;
-        System.out.println("\nThe " + employee + " salary is: ");
-        System.out.printf("U$: %.2f\n", hourValue);
+        System.out.printf("Enter a number: ");
+        input = scan.nextInt();
         scan.close();
+
+        if (input >= 0 ){
+            System.out.println("The number "+ input + " is positive: ");
+        }   else{
+            System.out.println("The number " + input + " in negative" );
+        }
     }
 }
