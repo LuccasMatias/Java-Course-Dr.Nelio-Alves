c83d1977f95aab84fd1bbd08f0deed5a0c9792ad
LuccasMatias <lucas.ma.goes@gmail.com>
2023-08-08 20:26:42 -0300

    03: repetitive structures (Switch-case || While)
    2). Write a program to read the (X,Y) coordinates of an undetermined number of points in the system
    Cartesian. For each point write the quadrant it belongs to. The algorithm will terminate when at least
    least one of two coordinates is NULL (in this situation without writing any message).

--- Lesson01/src/Main.java
+++ Lesson01/src/Main.java
@@ -2,18 +2,32 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        int password;
+        int axisX, axisY, cont = 0;
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Enter the password: ");
-
-        password = scan.nextInt();
-
         do {
-            System.out.println("Incorrect password \n");
-            password = scan.nextInt();
-        } while (password != 2002);
-        System.out.println("Correct password");
-        scan.close();
+            System.out.println("Enter the X axis: ");
+            axisX = scan.nextInt();
+            System.out.println("\nEnter the y axis: ");
+            axisY = scan.nextInt();
+
+            if (cont == 0 && (axisX != 0 && axisY != 0)){
+                System.out.println("First: [" + axisX + "][" + axisY + "]");
+                cont++;
+            }
+            else if (cont == 1 && (axisX != 0 && axisY != 0)){
+                System.out.println("Second: [" + axisX + "][" + axisY + "]");
+                cont++;
+            }
+            else if (cont == 2 && (axisX != 0 && axisY != 0)){
+                System.out.println("Third: [" + axisX + "][" + axisY + "]");
+                cont++;
+            }
+            else if (cont == 3 && (axisX != 0 && axisY != 0)){
+                System.out.println("Fourth: [" + axisX + "][" + axisY + "]");
+                cont++;
+            }
+        } while ((axisX != 0 && axisY != 0) || cont == 4);
+
     }
 }
