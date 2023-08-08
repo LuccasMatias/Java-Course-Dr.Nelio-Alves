ec5fd565d62bdd6e0d0a7245ecd40f4ca2dd83be
LuccasMatias <lucas.ma.goes@gmail.com>
2023-08-08 20:24:27 -0300

    01: Sequential Structures
    4). Write a program that reads the number of an employee, the number of hours worked, the amount he receives for
    hour and calculates the salary of that employee. Next, show the employee's number and salary, with two spaces
    decimals.

--- Lesson01/src/Main.java
+++ Lesson01/src/Main.java
@@ -2,18 +2,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int difference;
 
         Scanner scan = new Scanner(System.in);
+        int employee, workedHours;
+        double hourValue;
 
-        int a = scan.nextInt();
-        int b = scan.nextInt();
-        int c = scan.nextInt();
-        int d = scan.nextInt();
+        System.out.println("Enter the employee ID: ");
+        employee = scan.nextInt();
+        System.out.println("\nEnter the worked hours: ");
+        workedHours = scan.nextInt();
+        System.out.println("\nEnter the hour value");
+        hourValue = scan.nextDouble();
 
-        difference = a*b - c*d;
-
-        System.out.println("The difference between AB and CD is: " + difference);
+        hourValue = hourValue * workedHours;
+        System.out.println("\nThe " + employee + " salary is: ");
+        System.out.printf("U$: %.2f\n", hourValue);
         scan.close();
-
     }
+}
